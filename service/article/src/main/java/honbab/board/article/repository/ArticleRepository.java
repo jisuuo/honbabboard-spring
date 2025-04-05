0ef8079d3296ce59848284870a984a3cc1ac2a03
jissuo <jisu2298@gmail.com>
2025-04-05 15:15:50 +0900

    게시글CRUD 구현+ TEST 완료

---
+++ service/article/src/main/java/honbab/board/article/repository/ArticleRepository.java
@@ -0,0 +1,9 @@
+package honbab.board.article.repository;
+
+import honbab.board.article.entity.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ArticleRepository extends JpaRepository<Article, Long> {
+}
